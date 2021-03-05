f23a32e20f8a06fa3e001aac665ea3467d402c68
mharlow199 <79927160+mharlow199@users.noreply.github.com>
2021-03-05 12:15:51 -0600

    Add files via upload

---
+++ 2.5.11 Nim/Board.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+public class Board {
+
+    public static int pieces;
+    
+    public static void populate()
+    {
+        Board.pieces = (int) (Math.random() * 50);
+    }
+
+}
--- 2.5.11 Nim/Game.java
+++ 2.5.11 Nim/Game.java
@@ -1,1 +1,56 @@
-a
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Game {
+    
+    Scanner sc = new Scanner(System.in);
+
+    private String playerOne;
+    private String playerTwo;
+    private String lastPlayer;
+
+
+    public void play()
+    {
+        playerOne = newPlayer();
+        playerTwo = newPlayer();
+
+        System.out.printf("Players: %s, %s | Board pieces: %d", playerOne, playerTwo, Board.pieces);
+
+        firstMove();
+
+        while (Board.pieces > 0)
+        {
+            if (lastPlayer.equals(playerOne))
+            {
+                ;//TODO
+            }
+        }
+
+    }
+
+    private String newPlayer()
+    {
+        System.out.println("Enter a player name: ");
+        return sc.nextLine();
+    }
+    
+    private void firstMove()
+    {
+        if (Math.random() > 0.5d)
+        {
+            move(playerOne);
+            lastPlayer = playerOne;
+        } else {
+            move(playerTwo);
+            lastPlayer = playerTwo;
+        }
+    }
+
+    private String move(String player)
+    {
+        System.out.printf("%s, how many pieces do you want to take? %n", player);
+        return sc.nextLine();
+    }
+
+}
